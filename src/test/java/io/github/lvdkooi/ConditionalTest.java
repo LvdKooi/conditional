54406e900d7f079d870c1e82fb9daaed47378bd9
lvdkooi <lvvdkooi@hotmail.com>
2025-04-07 11:20:43 +0200

    Removing a SOUT

--- src/test/java/io/github/lvdkooi/ConditionalTest.java
+++ src/test/java/io/github/lvdkooi/ConditionalTest.java
@@ -138,8 +138,6 @@ class ConditionalTest {
         }
 
         private String returnHelloWorld() {
-            System.out.println("HELLO FROM ORELSEGET");
-
             return "hello world";
         }
 
